fbe52546dc02d2e35e26ddd004aaf1d92eccb823
Andrii Blyshchak <andrewblyshchak@gmail.com>
2021-01-23 19:39:43 +0200

    Updated files

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import express from 'express';
 import routes from './routes/routes';
 
-const app = express();
+export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(routes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
